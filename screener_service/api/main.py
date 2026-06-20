6fdb574e00daecbd3af44ec364e24e29bb17f519
ryanzhxu <ryan.xu282@hotmail.com>
2026-06-19 23:27:02 -0700

    feat: deploy web-ui to Render + production CORS + env

--- screener_service/api/main.py
+++ screener_service/api/main.py
@@ -15,7 +15,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://finance-web-ui.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
